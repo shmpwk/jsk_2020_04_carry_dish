4975d4a31ae44999bc0ea0caec2e06a9d9f66cd5
Shumpei Wakabayashi <wakabayashi@jsk.imi.i.u-tokyo.ac.jp>
2020-08-19 15:13:15 +0900

    data collection

---
+++ scripts/first_logger.py
@@ -0,0 +1,14 @@
+# This file is a test for loggerpy.
+
+if __name__ == '__main__':
+    from loggerpy import *
+
+    configure()
+
+    logger = get_logger('first')
+
+    logger.debug('Debug test')
+    logger.info('Info test')
+    logger.warning('Warning test')
+    logger.error('Error test')
+    logger.critical('Critical test')
