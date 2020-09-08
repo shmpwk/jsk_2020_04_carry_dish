7936971c49d4079c97732a1eedb71b5f5a7792d3
Shumpei Wakabayashi <wakabayashi@jsk.imi.i.u-tokyo.ac.jp>
2020-09-08 20:27:48 +0900

    write csv

--- scripts/pointcloud_to_csv.py
+++ scripts/pointcloud_to_csv.py
@@ -4,13 +4,19 @@
 from sensor_msgs.msg import PointCloud2
 from sensor_msgs import point_cloud2
 import rospy
+import csv
 
 def callback_pointcloud(data):
     assert isinstance(data, PointCloud2)
     gen = point_cloud2.read_points(data, field_names = ("x", "y", "z"), skip_nans=True)
     print type(gen)
-    for p in gen:
-      print p  # type depends on your data type, first three entries are probably x,y,z
+    with open('pointcloudxyz.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerows(gen) #shape(64751, 3)
+
+
+    #for p in gen:
+    #  print p 
 
 if __name__ == '__main__':
     rospy.init_node('pointcloud_to_csv')
