45e9901ab5ba1a334109550f50b9bffb409a32f0
Shumpei Wakabayashi <wakabayashi@jsk.imi.i.u-tokyo.ac.jp>
2020-09-11 19:43:14 +0900

    edit service

--- scripts/pub_grasp_point.py
+++ scripts/pub_grasp_point.py
@@ -6,28 +6,25 @@
 # publish grasp point by service 
 
 import rospy
-import geometry_msgs.msg import Point
+from geometry_msgs.msg import Point
 from sensor_msgs.msg import PointCloud2
 from sensor_msgs import point_cloud2 
 
 def choose_point_callback(msg):
     gen = point_cloud2.read_points(data, field_names = ("x", "y", "z"), skip_nans=True)
 
-
-    point = Point()
+    #point = Point()
+    return msg
 
 
 
 # get depth and get grasp point 
 def handle_grasp_point(req):
-    #req.boxからその部分のpoint cloud を得る
     #subscribe edge pointcloud data
-
     try:
-        rospy.Subscriber('/edgepoint2', PointCloud2, choose_point_callback, queue_size=1)
-
-    point = choose_point()
-    return GraspPointResponse(point)
+        rospy.Subscriber('/camera/depth_registered/points', PointCloud2, choose_point_callback, queue_size=1)
+        rospy.spin()
+    except rospy.ROSInterruptException: pass
 
 def grasp_point_server():
     rospy.init_node('grasp_point_server')
