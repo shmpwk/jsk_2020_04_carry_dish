128dcb338bf78bb5f1839e2e17e629f866d86e7e
Shumepei Wakabayashi <wakabayashi@jsk.imi.i.u-tokyo.ac.jp>
2020-09-13 16:10:00 +0900

    publish pose from pcl

--- scripts/pub_grasp_point.py
+++ scripts/pub_grasp_point.py
@@ -10,19 +10,47 @@ from geometry_msgs.msg import Point
 from sensor_msgs.msg import PointCloud2
 from sensor_msgs import point_cloud2 
 from geometry_msgs.msg import Pose
+import tf
 
 def choose_point_callback(data):
     assert isinstance(data, PointCloud2)
+
+    """
+    Randomly choose position (x, y, z) data from point cloud
+    """
     gen = point_cloud2.read_points(data, field_names = ("x", "y", "z"), skip_nans=True)
+    A = np.arrange(3).reshape(1,3)
+    for l in gen:
+        l = np.array(l)
+        l = l.reshape(1,3)
+        A = np.append(A, l, axis=0)
+    idx = np.random.randint(10, size=1) #To do : change 10 to data size
+    Ax = A[idx, 0]
+    Ay = A[idx, 1]
+    Az = A[idx, 2]
+
+    """
+    Romdomly choose rotation theta from 0, 45, 90. (currently, 90 for test).
+    Other roatation angle is fixed toward middle point.
+
+    """
+
+    theta = 0
+    phi = 1.2
+    psi = -1.54
+    q = tf.transformations.quaternion_from_euler((theta, phi, psi))
+
+
 
     pose = Pose()
-    pose.position.x = 5
-    pose.position.y = 4
-    pose.position.z = 3
-    pose.orientation.x = 1
-    pose.orientation.y = 1
-    pose.orientation.z = 1
-    pose.orientation.w = 1
+    pose.position.x = Ax
+    pose.position.y = Ay
+    pose.position.z = Az 
+
+    pose.orientation.x = q[0]
+    pose.orientation.y = q[1]
+    pose.orientation.z = q[2]
+    pose.orientation.w = q[3]
     
     pub.publish(pose)
 
@@ -38,6 +66,7 @@ if __name__=="__main__":
         rospy.spin()
     except rospy.ROSInterruptException: pass
 
+"""
 def grasp_point_server():
     rospy.init_node('grasp_point_server')
     s = rospy.Service('grasp_point', GetGraspPoint, handle_grasp_point)
@@ -47,3 +76,4 @@ def grasp_point_server():
 #if __name__=="__main__":
 #    #grasp_point_server()
 #    handle_grasp_point()
+"""
