ffbad17af86a4c18c66fdbca8ffc26bab84e5929
Shumpei Wakabayashi <wakabayashi@jsk.imi.i.u-tokyo.ac.jp>
2020-10-07 19:34:53 +0900

    try pkl and virtual camera

--- node_scripts/depth_converter.py
+++ node_scripts/depth_converter.py
@@ -13,25 +13,25 @@ import pickle
 from absl import app
 from absl import flags
 import datetime
+import time
+import numpy as np
+import pickle
+import os 
+import time 
+import matplotlib.pyplot as plt
 
-FLAGS = flags.FLAGS
+#FLAGS = flags.FLAGS
 
-flags.DEFINE_string(
-        'depth_topic', '/kinect_head_remote/depth_registered/image_rect', 'depth topic name')
+#flags.DEFINE_string(
+#        'depth_topic', '/head_mount_kinect/depth/image_raw', 'depth topic name')
 
 def ImageCallback(depth_data):
-    WIDTH = 50
-    HEIGHT = 50 
-    bridge = CvBridge()
-    try:
-        #color_image = bridge.imgmsg_to_cv2(rgb_data, 'passthrough')
-        depth_image = bridge.imgmsg_to_cv2(depth_data, 'passthrough')
-    except CvBridgeError, e:
-        rospy.logerr(e)
+    WIDTH = 100
+    HEIGHT = 100 
+    depth_image = depth_data
 
     #color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB) 
     #h, w, c = color_image.shape
-    #print("h,w,c",h,w,c)
     h, w = depth_image.shape
 
     x1 = (w / 2) - WIDTH
@@ -40,7 +40,8 @@ def ImageCallback(depth_data):
     y2 = (h / 2) + HEIGHT
     sum = 0.0
     points = []
-    depth_data = []
+    #depth_data = []
+    depth_data = np.empty(0)
 
     for i in range(y1, y2):
         for j in range(x1, x2):
@@ -49,25 +50,36 @@ def ImageCallback(depth_data):
             if depth_image.item(i,j) == depth_image.item(i,j):
                 #point = Point(i, j, depth_image.item(i,j))
                 #points.append(point)
-                depth_data.append(depth_image.item(i,j))
+                #depth_data.append(depth_image.item(i,j))
+                depth_data = np.append(depth_data, depth_image.item(i,j))
+            else:
+                depth_data = np.append(depth_data, 0)
 
     ave = sum / ((WIDTH * 2) * (HEIGHT * 2)) #average distance 
     now = datetime.datetime.now()
-    print(len(depth_data))
-    filename = 'Data/depth_image/depth_image_' + now.strftime('%Y%m%d_%H%M%S') + '.pkl'
+    filename = 'Data/gazebo_depth_image/depth_image_' + now.strftime('%Y%m%d_%H%M%S') + '.pkl'
     with open(filename, 'wb') as f:
         pickle.dump(depth_data, f) #datasize=480
+    time.sleep(2)
     print("depth saved at node script")
+    print("depthdata",depth_data.reshape((200,200)).shape)
+    print("depthimage", depth_image.shape)
 
     cv2.normalize(depth_image, depth_image, 0, 1, cv2.NORM_MINMAX)
     #cv2.namedWindow("color_image")
     cv2.namedWindow("depth_image")
     #cv2.imshow("color_image", color_image)
+    #cv2.imshow("depth_image", depth_data)
     cv2.imshow("depth_image", depth_image)
-    cv2.waitKey(10)
+    #plt.imshow(depth_data)
+    #plt.show()
+    cv2.waitKey(1000)
+    cv2.waitKey(1)
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
 
-def main(argv):
-    rospy.init_node('depth_estimater', anonymous=True)
+#def main():
+if __name__ == '__main__':
     """ 
     sub_rgb = message_filters.Subscriber("/head_mount_kinect/rgb/image_raw",Image)
     sub_depth = message_filters.Subscriber("/head_mount_kinect/depth/image_raw",Image)
@@ -77,15 +89,17 @@ def main(argv):
     #pub = rospy.Publisher("/depth_crds", Point)
     rospy.spin()
     """
-    try:
-        while not rospy.is_shutdown():
-            topic_name = "{}".format(FLAGS.depth_topic) 
-            data = rospy.wait_for_message(topic_name,Image)
-            #data = rospy.wait_for_message("/head_mount_kinect/depth/image_raw", Image)
-            ImageCallback(data)
-            break
-    except rospy.ROSInterruptException: pass
+    depth_path = "Data/depth_data"
+    depth_key = '.pkl'
+    for dir_name, sub_dirs, files in os.walk(depth_path): 
+        for f in files:
+            if depth_key == f[-len(depth_key):]:
+                with open(os.path.join(dir_name, f), 'rb') as f:
+                    data = pickle.load(f)
+                    ImageCallback(data)
 
+"""
 if __name__ == '__main__':
     print("=============================================started!!")
-    app.run(main)
+    #app.run(main)
+    main"""
--- scripts/depth_pickle_load.py
+++ scripts/depth_pickle_load.py
@@ -7,18 +7,17 @@ from PIL import Image
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
 
-grasp_dataset = np.empty((0,40000))
+grasp_dataset = np.empty((0,10000))
 
-for file in os.listdir(".ros/Data/gazebo_depth_image"):
-    with open (".ros/Data/gazebo_depth_image/" + file, "rb") as f:
+for file in os.listdir(".ros/Data/depth_image"):
+    with open (".ros/Data/depth_image/" + file, "rb") as f:
         ff = pickle.load(f)
-        ff = np.array(ff).reshape((1, 40000))
+        ff = np.array(ff).reshape((1, 10000))
         grasp_dataset = np.append(grasp_dataset, ff, axis=0)
 print(grasp_dataset.shape)
-ims = grasp_dataset.reshape((10, 200, 200))
+ims = grasp_dataset.reshape((1, 100, 100))
 plt.imshow(np.transpose(ims[0, :, :]))
 plt.show()
-
 
 """
 mm =preprocessing.MinMaxScaler()
