dc01a51a6a24e887a2a78f667431894c9e5f6392
Shumpei Wakabayashi <wakabayashi@jsk.imi.i.u-tokyo.ac.jp>
2020-10-18 17:37:59 +0900

    train fail by depth so try rgbd

--- scripts/rgbd_grasp_system.py
+++ scripts/rgbd_grasp_system.py
@@ -103,12 +103,50 @@ class MyDataset(Dataset):
         """
         depth_path = "Data/depth_data"
         self.depth_dataset = np.empty((0,230400))
+        self.gray_dataset = np.empty((0,230400))
         depth_key = 'extract_depth_image.pkl'
         color_key = 'extract_color_image.pkl'
+        t_cnt = 0
         tmp_cnt = 0
         for d_dir_name, d_sub_dirs, d_files in sorted(os.walk(depth_path)): 
             for df in sorted(d_files):
-                #if depth_key == df[-len(depth_key):]:
+                if color_key == df[-len(color_key):]:
+                    with open(os.path.join(d_dir_name, df), 'rb') as f:
+                        fff = pickle.load(f)
+                        color_image = fff
+                        WIDTH = 240
+                        HEIGHT = 240
+                        """
+                        bridge = CvBridge()
+                        try:
+                            color_image = bridge.imgmsg_to_cv2(ff, 'passthrough')
+                        except CvBridgeError, e:
+                            rospy.logerr(e)
+                        """
+                        im = fff.reshape((480,640,3))
+                        im_gray = 0.299 * im[:, :, 0] + 0.587 * im[:, :, 1] + 0.114 * im[:, :, 2]
+                        h, w = im_gray.shape
+                        x1 = (w / 2) - WIDTH
+                        x2 = (w / 2) + WIDTH
+                        y1 = (h / 2) - HEIGHT
+                        y2 = (h / 2) + HEIGHT
+                        gray_data = np.empty((0,230400))
+
+                        for i in range(y1, y2):
+                            for j in range(x1, x2):
+                                if im_gray.item(i,j) == im_gray.item(i,j):
+                                    gray_data = np.append(gray_data, im_gray.item(i,j))
+                                else:
+                                    gray_data = np.append(gray_data, 0)
+                                            
+                        gray_data = np.array(gray_data).reshape((1, 230400))
+                        #self.depth_dataset = np.append(self.depth_dataset, depth_data, axis=0)
+                        if (t_cnt == 1 or t_cnt == 3):
+                            self.gray_dataset = np.append(self.gray_dataset, np.tile(gray_data, (50, 1)).reshape(50, 230400), axis=0)
+                        else:
+                            self.gray_dataset = np.append(self.gray_dataset, np.tile(gray_data, (20, 1)).reshape(20, 230400), axis=0)
+                        t_cnt += 1
+
                 if depth_key == df[-len(depth_key):]:
                     with open(os.path.join(d_dir_name, df), 'rb') as f:
                         ff = pickle.load(f)
@@ -150,6 +188,10 @@ class MyDataset(Dataset):
 
                         tmp_cnt += 1
         self.depth_dataset = self.depth_dataset.reshape((160, 1, 480, 480))
+        self.gray_dataset = self.gray_dataset.reshape((160, 1, 480, 480))
+
+        self.gray_depth_dataset = np.concatenate([self.depth_dataset, self.gray_dataset], 1)
+        print("gray depth dataset", self.gray_depth_dataset.shape)
         print("Finished loading all depth data")
         
         # grasp point data size : 10 * 6(4)   
@@ -196,7 +238,7 @@ class MyDataset(Dataset):
                     self.judge_dataset = np.append(self.judge_dataset, int(n))
         print("Finished loading judge data")
 
-        print(self.depth_dataset.shape)
+        print(self.gray_depth_dataset.shape)
         print(self.grasp_dataset.shape)
         print(self.judge_dataset.shape)
 
@@ -204,16 +246,9 @@ class MyDataset(Dataset):
         return self.datanum #should be dataset size / batch size
 
     def __getitem__(self, idx):
-        x = self.depth_dataset[idx]
+        x = self.gray_depth_dataset[idx]
         y = self.grasp_dataset[idx]
         c = self.judge_dataset[idx]
-        """
-        y = pd.read_csv(self.csvfiles[idx], header=None),
-        yy = np.array(yy, dtype=np.float32)[0]
-        x = self.transform(x) if self.transform else x
-        c = yy[1:]
-        y = yy[:1]
-        """
         x = torch.from_numpy(x).float()
         y = torch.from_numpy(y).float()
         c = torch.from_numpy(np.array(c)).float()
@@ -243,7 +278,7 @@ class Net(nn.Module):
         """
 
         # dynamics-net (icra2019ÇÃïRÇ∆Ç©è_ìÓï®ÇëÄÇÈÇ‚Ç¬) by Mr. Kawaharazuka
-        self.conv1 = nn.Conv2d(1, 4, 3, 2, 1)
+        self.conv1 = nn.Conv2d(2, 4, 3, 2, 1)
         self.cbn1 = nn.BatchNorm2d(4)
         self.conv2 = nn.Conv2d(4, 8, 3, 2, 1)
         self.cbn2 = nn.BatchNorm2d(8)
@@ -335,7 +370,7 @@ class GraspSystem():
         self.train_optimizer = optim.Adam(self.model.parameters(), lr=0.001)
         #self.train_optimizer = optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9)
         #self.test_optimizer = optim.SGD(self.model
-        summary(self.model, [(1, 480, 480), (4,)])
+        summary(self.model, [(2, 480, 480), (4,)])
 
     def get_batch_train():
         pass
