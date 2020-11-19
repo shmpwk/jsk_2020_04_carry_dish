c76de34dbec1db4436fd8b12e353b40deeef22de
Shumpei Wakabayashi <wakabayashi@jsk.imi.i.u-tokyo.ac.jp>
2020-11-19 21:50:59 +0900

    load 64*64 file

--- scripts/depth_pickle_load.py
+++ scripts/depth_pickle_load.py
@@ -37,14 +37,15 @@ def colorize_depth(depth, min_value=None, max_value=None, dtype=np.uint8):
     return colorized
 
 depth_path = "Data/depth_data"
-depth_dataset = np.empty((0,230400))
+#depth_dataset = np.empty((0,230400))
 #depth_key = 'extract_depth_image.pkl'
 #color_key = 'extract_color_image.pkl'
-depth_key = '.pkl'
+depth_key = 'heightmap_image.pkl'
+color_key = 'extract_color_image.pkl'
 for d_dir_name, d_sub_dirs, d_files in sorted(os.walk(depth_path)): 
     for df in sorted(d_files):
         #if depth_key == df[-len(depth_key):]:
-        if depth_key == df[-len(depth_key):]:
+        if color_key == df[-len(color_key):]:
             with open(os.path.join(d_dir_name, df), 'rb') as f:
                 ff = pickle.load(f)
                 depth_image = ff
@@ -57,11 +58,18 @@ for d_dir_name, d_sub_dirs, d_files in sorted(os.walk(depth_path)):
                 except CvBridgeError, e:
                     rospy.logerr(e)
                 """
-                
+                im = ff.reshape((480,640,3))
+                pil_im = Image.fromarray(np.uint8(im))
+                pil_im = pil_im.resize((129, 172))
+                im = np.asarray(pil_im)
+                im_gray = 0.299 * im[:, :, 0] + 0.587 * im[:, :, 1] + 0.114 * im[:, :, 2]
+
+                """
                 im = ff.reshape((128,128,2))
                 depth_image = im[:, :, 0]
                 #im_gray = 0.299 * im[:, :, 0] + 0.587 * im[:, :, 1] + 0.114 * im[:, :, 2]
-                #depth_image = im_gray
+                """
+                depth_image = im_gray
                 
                 print("depth image shape", depth_image.shape)
                 h, w = depth_image.shape
@@ -94,7 +102,47 @@ for d_dir_name, d_sub_dirs, d_files in sorted(os.walk(depth_path)):
                 plt.imshow(np.transpose(ims[0, :, :]))
                 plt.show()
                 """
-                print("printed")
+        if depth_key == df[-len(depth_key):]:
+            with open(os.path.join(d_dir_name, df), 'rb') as f:
+                ff = pickle.load(f)
+                depth_image = ff
+                WIDTH = 64#240
+                HEIGHT = 64#240
+                """
+                bridge = CvBridge()
+                try:
+                    depth_image = bridge.imgmsg_to_cv2(ff, 'passthrough')
+                except CvBridgeError, e:
+                    rospy.logerr(e)
+                """
+                """
+                im = ff.reshape((480,640,3))
+                im_gray = 0.299 * im[:, :, 0] + 0.587 * im[:, :, 1] + 0.114 * im[:, :, 2]
+                depth_image = im_gray
+                """
+                im = ff.reshape((128,128,2))
+                depth_image = im[:, :, 0]
+                h, w = depth_image.shape
+                x1 = (w / 2) - WIDTH
+                x2 = (w / 2) + WIDTH
+                y1 = (h / 2) - HEIGHT
+                y2 = (h / 2) + HEIGHT
+                depth_data = np.empty((0,16384)) #230400))
+
+                for i in range(y1, y2):
+                    for j in range(x1, x2):
+                        if depth_image.item(i,j) == depth_image.item(i,j):
+                            depth_data = np.append(depth_data, depth_image.item(i,j))
+                        else:
+                            depth_data = np.append(depth_data, 0)
+                depth_data = np.array(depth_data).reshape((1, 16384)) #230400))
+                ims = depth_data.reshape((1, 128, 128)) #480, 480))
+                print(ims)
+                #im = colorize_depth(ims)
+                #plt.imshow(ims)
+                #plt.show()
+                plt.imshow(np.transpose(ims[0, :, :]))
+                plt.show()
 
 """
 for file in os.listdir(".ros/Data/depth_image"):
@@ -102,7 +150,7 @@ for file in os.listdir(".ros/Data/depth_image"):
         ff = pickle.load(f)
         ff = np.array(ff).reshape((1, 480*640))
         grasp_dataset = np.append(grasp_dataset, ff, axis=0)
-        """
+"""
 
 """
 mm =preprocessing.MinMaxScaler()
