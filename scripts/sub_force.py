8df31bd99e4193039f17b3f44e1d3f44888cb8b1
Shumpei Wakabayashi <wakabayashi@jsk.imi.i.u-tokyo.ac.jp>
2021-02-23 21:54:28 +0900

    seq grasp data collection

---
+++ scripts/sub_force.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import rospy
+import tf
+
+from jsk_recognition_msgs.msg import *
+from geometry_msgs.msg import *
+from std_msgs.msg import *
+
+def wrench_cb(msg):
+    x_force =  msg.wrench.force.x
+    y_force =  msg.wrench.force.y
+    z_force =  msg.wrench.force.z
+    xy = x_force**2 + y_force**2
+    yz = y_force**2 + z_force**2
+    zx = z_force**2 + x_force**2
+    #print("xy", xy)
+    #print("yz", yz)
+    #print("zx", zx)
+    print (y_force)
+    if y_force > 3:
+        pub.publish("True")
+        print("touch")
+    else :
+        pub.publish(!"False")
+        print("nothing")
+    print("==========")
+
+rospy.init_node('touch_detect')
+position_sub = rospy.Subscriber('/right_endeffector/wrench', WrenchStamped, wrench_cb)
+pub = rospy.Publisher(
+    'judge_contact', Bool, queue_size=1)
+rospy.spin()
+    
